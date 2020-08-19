b801a9e09dd5eb117da3fd5ec00ffe910bd37c07
berenzorn <berenzorn@gmail.com>
2020-08-19 13:05:57 +0300

    new option in hashgen

--- hashgen.py
+++ hashgen.py
@@ -7,7 +7,10 @@ from pathlib import Path
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("source")
-    parser.add_argument("-g", "--generate", action="store_true", help="Generate .sha1 for source")
+    # parser.add_argument("-g", "--generate", action="store_true", help="Generate .sha1 for source")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-a", "--generate-all", dest="generall", action="store_true", help="Generate .sha1 for source")
+    group.add_argument("-n", "--generate-new", dest="genernew", action="store_true", help="Generate .sha1 for new files")
     parser.add_argument("-c", "--clear", action="store_true", help="Clear .sha1 orphans")
     parser.add_argument("-q", "--quiet", action="store_true", help="Quiet mode, no output")
     parser.add_argument("-l", type=str, dest="log", metavar=" LOG", help="Write output to log file")
@@ -23,8 +26,8 @@ if __name__ == '__main__':
 
     buffer_size = 1024**2 * 100
 
-    if not args.clear:
-        args.generate = True
+    if not args.generall:
+        args.genernew = True
 
     if args.log:
         logging.debug("-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-")
@@ -32,11 +35,12 @@ if __name__ == '__main__':
         logging.debug(f"args.quiet={args.quiet}")
 
     orphans = []
+    news = []
     if Path(args.source).exists():
         source_list = [file.name for file in lib.file_array(args.source, True)]
-        if args.clear:
-            sha1_list = [file.name[:-5] for file in lib.file_array(args.source, False)]
-            orphans = sorted(list(set(sha1_list) - set(source_list)))
+        sha1_list = [file.name[:-5] for file in lib.file_array(args.source, False)]
+        orphans = sorted(list(set(sha1_list) - set(source_list)))
+        news = sorted(list(set(source_list) - set(sha1_list)))
     else:
         lib.print_out("No such source folder.", args.quiet, args.log)
         raise SystemExit(0)
@@ -47,10 +51,16 @@ if __name__ == '__main__':
             msg = "Cleaning .sha1 orphans"
             lib.sha1_remove(args.source, orphans, msg, args.quiet, args.log)
 
-    if args.generate:
+    def regen_sha1(file_array):
         lib.print_out("", args.quiet, args.log)
-        for x in source_list:
+        for x in file_array:
             try:
                 lib.sha1_write_queue(args.source, x, buffer_size, args.quiet, args.log)
             except PermissionError:
                 lib.print_out("IO Error, no permission to read/write", args.quiet, args.log)
+
+    if args.generall:
+        regen_sha1(source_list)
+
+    if args.genernew:
+        regen_sha1(news)
